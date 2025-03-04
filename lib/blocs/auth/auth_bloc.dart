8b53ad2cb22ed746f9f42e2057b42e7af474b8c7
Christopher Oderic YASSINGUEZO <oyassinguezo@hyperion-dev.com>
2025-03-04 18:04:00 +0100

    Bloc Authentification terminé

--- lib/blocs/auth/auth_bloc.dart
+++ lib/blocs/auth/auth_bloc.dart
@@ -1,13 +1,62 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:get_country_apk/model/user_model.dart';
+import 'package:get_country_apk/repositories/user_repository.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserRepository _userRepository;
+
+  AuthBloc({required UserRepository userRepository})
+      : _userRepository = userRepository,
+      super(AuthInitial()) {
+      on<SignUpRequestedEvent>(_onSignUp);
+      on<SignInRequestedEvent>(_onSignIn);
+      on<SignOutRequestedEvent>(_onSignOut);
+  }
+
+  FutureOr<void> _onSignUp(SignUpRequestedEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final user = await _userRepository.singUp(
+        email: event.email,
+        password: event.password,
+        username: event.username 
+      );
+      if (user != null) {
+        emit(AuthAuthenticated(user: user));
+      } else {
+        emit(AuthError(message: 'Echec de l\'inscription'));
+      }
+    } catch (e) {
+      emit(AuthError(message: e.toString()));
+    }
+  }
+
+  FutureOr<void> _onSignIn(SignInRequestedEvent event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final user = await _userRepository.singIn(
+        email: event.email,
+        password: event.password
+      );
+      if (user != null) {
+        emit(AuthAuthenticated(user: user));
+      } else {
+        emit(AuthError(message: 'Echec de la connexion'));
+      }
+    } catch (e) {
+      emit(AuthError(message: e.toString()));
+    }
+
+  }
+
+  FutureOr<void> _onSignOut(SignOutRequestedEvent event, Emitter<AuthState> emit) async {
+    await _userRepository.signOut();
+    emit(AuthInitial());
   }
 }
--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -1,8 +1,40 @@
 part of 'auth_bloc.dart';
 
-sealed class AuthEvent extends Equatable {
+abstract class AuthEvent extends Equatable {
   const AuthEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class SignUpRequestedEvent extends AuthEvent {
+  final String email;
+  final String password;
+  final String username;
+
+  const SignUpRequestedEvent({
+    required this.email, 
+    required this.password, 
+    required this.username
+  });
+
+  @override
+  List<Object> get props => [email, password, username];
+}
+
+class SignInRequestedEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  const SignInRequestedEvent({
+    required this.email, 
+    required this.password
+  });
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class SignOutRequestedEvent extends AuthEvent {
+  
 }
