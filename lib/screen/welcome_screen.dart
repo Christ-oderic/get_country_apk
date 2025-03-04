e9a4b590c8c989945d83851f2a7442ea173b54f5
Christopher Oderic YASSINGUEZO <oyassinguezo@hyperion-dev.com>
2025-03-04 16:53:16 +0100

    Mise en place final de la page d'acceuil

--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_country_apk/blocs/welcome/welcome_bloc.dart';
+import 'package:get_country_apk/common/colors.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -21,18 +22,27 @@ class WelcomeScreen extends StatelessWidget {
                   children: [
                     PageView(
                       controller: pageController,
-                      // onPageChanged: (index) {
-                        
-                      // },
+                      onPageChanged: (index) {
+                        context.read<WelcomeBloc>().add(UpdateWelcomePageEvent(index));
+                      },
                       children: [
-                        _page(1, context, 'Welcome', 'assets/earthFromSpace.png', 'Next',pageController),
-                        _page(2, context, 'Welcome', 'assets/worldMap.png', 'Next', pageController),
-                        _page(3, context, 'Welcome', 'assets/earthFromSpaceOnAfrica.png', 'Get Started',pageController),
+                        _page(1, context, 
+                        "BIENVENUE DANS GEOQUIZ!","assets/earthFromSpace.png",
+                        "Es-tu prêt à explorer le monde et à défier tes connaissances sur les pays ?", 
+                        "Next",pageController),
+                        _page(2, context, 
+                        'Choisis un mode de jeu!','assets/worldMap.png',
+                        'Réponds aux questions le plus vite possible ⏳,Gagne des points et grimpe dans le classement ! 🏆', 
+                         'Next', pageController),
+                        _page(3, context, 
+                        'Es-tu prêt à relever le défi ?','assets/earthFromSpaceOnAfrica.png',
+                        ' Let\'s go 🚀', 
+                         'Get Started',pageController),
                       ],
                       
                     ),
                     Positioned(
-                      bottom: 80.h,
+                      bottom: 40,
                       child: BlocBuilder<WelcomeBloc, WelcomeState>(
                         builder: (context, state) {
                           if (state is WelcomeLoadedState){
@@ -41,13 +51,10 @@ class WelcomeScreen extends StatelessWidget {
                               dotsCount: 3,
                               mainAxisAlignment: MainAxisAlignment.center,
                               decorator: DotsDecorator(
-                                color: Colors.grey,
-                                activeColor: Colors.black,
+                                color: AppColors.primaryThirdElementText,
+                                activeColor: AppColors.primaryElement,
                                 size: const Size.square(10.0),
                                 activeSize: const Size(18.0, 8.0),
-                                activeShape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                ),
                         ),
                       );
                     }
@@ -70,29 +77,47 @@ class WelcomeScreen extends StatelessWidget {
   }
 
 
-  Widget _page(int index, BuildContext context, String title, String imagePath, buttonText, pageController) {
+  Widget _page(int index, BuildContext context, String title, String imagePath,  String subtitle, String buttonText, pageController) {
   return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Padding(padding: EdgeInsets.symmetric(horizontal: 20.w),
-      child:  Image.asset(
-        imagePath,
-        width: double.maxFinite,
-        height: 300.h,
-        fit: BoxFit.contain,
+      Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: SizedBox(width: 345.w,height: 345.w,
+        child:Center(
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+          ),
+        ),),
       ),
-      ),
-     SizedBox(height: 20.h),
-      Text(
-        title,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.normal,
-        ),
-      ),
-     SizedBox(height: 40.h),
-
+      Column(
+        children: [
+          SizedBox(height: 20.h),
+          Text(
+            title,
+            style: TextStyle(
+            color: AppColors.primaryText,
+            fontSize: 26,
+            fontWeight: FontWeight.normal,
+            ),
+          ),
+          SizedBox(height: 20.h),
+          Container(
+            width: 375.w,
+            padding: EdgeInsets.only(left: 30.w, right: 30.w),
+            child: Center(
+              child: Text(
+                subtitle,
+                style: TextStyle(
+                  color: AppColors.primarySecondaryElementText,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal
+                ),
+              ),
+            ),
+          ),
+        ]
+      ),  
       GestureDetector(
         onTap: () {
           if (index < 3) {
@@ -107,17 +132,17 @@ class WelcomeScreen extends StatelessWidget {
         },
         child: Container(
           margin: const EdgeInsets.only(top: 100, left: 25, right: 25),
-          width: 325,
-          height: 50,
+          width: 325.w,
+          height: 50.h,
           decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(10),
+            color: AppColors.primaryElement,
+            borderRadius: BorderRadius.all(Radius.circular(15.w)),
             boxShadow: [
               BoxShadow(
                 color: Colors.grey,
                 spreadRadius: 1,
                 blurRadius: 2,
-                offset: const Offset(0, 2),
+                offset: const Offset(0, 1),
               )
             ],
           ),
